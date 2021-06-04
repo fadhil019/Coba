13efc3e5ad609ff29ec96088dff2ff39ffdd8039
sdhiemazp <sdhiemazp@gmail.com>
2021-06-04 22:50:00 +0700

    update
    terbaru.. benahi rekap data dan pengecekan request

--- app/Imports/ImportDataPasien.php
+++ app/Imports/ImportDataPasien.php
@@ -50,88 +50,94 @@ class ImportDataPasien implements ToCollection, WithStartRow
         	$deskripsi_tindakan = $row[11];
         	$jp = $row[13];
 
-        	$pasien = DB::table('data_pasien')->where('nama_pasien', $nama_pasien)->first();
-			if($pasien == null) {
-				$pasien = new DataPasien();
-				$pasien->nama_pasien = $nama_pasien;
-				$pasien->penjamin = $penjamin;
-				$pasien->save();
-			}
-        	$id_data_pasien = $pasien->id_data_pasien;
-
-			if (strpos($nama_dokter_perawat, "dr") !== false) {
-				$bagian = "";
-				if (strpos($nama_dokter_perawat, "sp") !== false || strpos($nama_dokter_perawat, "Sp") !== false) {
-					$bagian = "Spesialis";
+        	$sep_pasien = DB::table('data_keuangan_pasien')
+			->where('no_sep_keuangan_pasien', $no_sep)
+			->where('id_periode', session('id_periode'))
+			->first();
+			if($sep_pasien != null) {
+				$pasien = DB::table('data_pasien')->where('nama_pasien', $nama_pasien)->first();
+				if($pasien == null) {
+					$pasien = new DataPasien();
+					$pasien->nama_pasien = $nama_pasien;
+					$pasien->penjamin = $penjamin;
+					$pasien->save();
+				}
+				$id_data_pasien = $pasien->id_data_pasien;
+	
+				if (strpos($nama_dokter_perawat, "dr") !== false) {
+					$bagian = "";
+					if (strpos($nama_dokter_perawat, "sp") !== false || strpos($nama_dokter_perawat, "Sp") !== false) {
+						$bagian = "Spesialis";
+					} else {
+						$bagian = "Umum";
+					}
+	
+					$dokter = DB::table('dokter')->where('nama_dokter', $nama_dokter_perawat)->first();
+					if($dokter == null) {
+						$dokter = new Dokter();
+						$dokter->nama_dokter = $nama_dokter_perawat;
+						$dokter->bagian = $bagian;
+						$dokter->id_users = Auth::user()->id_users;
+						$dokter->id_ruangan = session('id_ruangan');
+						$dokter->save();
+					}
 				} else {
-					$bagian = "Umum";
+					if($nama_dokter_perawat != "-" && $nama_dokter_perawat != "") {
+						$karyawan_perawat = DB::table('karyawan_perawat')->where('nama', $nama_dokter_perawat)->first();
+						if($karyawan_perawat == null) {
+							$karyawan_perawat = new KaryawanPerawat();
+							$karyawan_perawat->nama = $nama_dokter_perawat;
+							$karyawan_perawat->jabatan = "Karyawan";
+							$karyawan_perawat->id_users = Auth::user()->id_users;
+							$karyawan_perawat->id_ruangan = session('id_ruangan');
+							$karyawan_perawat->save();
+						}
+					}
 				}
-
-				$dokter = DB::table('dokter')->where('nama_dokter', $nama_dokter_perawat)->first();
-				if($dokter == null) {
-					$dokter = new Dokter();
-					$dokter->nama_dokter = $nama_dokter_perawat;
-					$dokter->bagian = $bagian;
-					$dokter->id_users = Auth::user()->id_users;
-					$dokter->id_ruangan = session('id_ruangan');
-					$dokter->save();
-				}
-			} else {
-				if($nama_dokter_perawat != "-" && $nama_dokter_perawat != "") {
-					$karyawan_perawat = DB::table('karyawan_perawat')->where('nama', $nama_dokter_perawat)->first();
-					if($karyawan_perawat == null) {
-						$karyawan_perawat = new KaryawanPerawat();
-						$karyawan_perawat->nama = $nama_dokter_perawat;
-						$karyawan_perawat->jabatan = "Karyawan";
-						$karyawan_perawat->id_users = Auth::user()->id_users;
-						$karyawan_perawat->id_ruangan = session('id_ruangan');
-						$karyawan_perawat->save();
+	
+				if($row[0] != "") {
+					$transaksi = new Transaksi();
+					$transaksi->id_data_pasien = $id_data_pasien;
+					$transaksi->no_sep = $no_sep;
+					$transaksi->reg_type = $reg_type;
+					$transaksi->id_users = Auth::user()->id_users;
+					$transaksi->id_periode = session('id_periode');
+					$transaksi->id_ruangan = session('id_ruangan');
+					$transaksi->save();
+	
+					$id_transaksi = $transaksi->id_transaksi;
+	
+					$data_deskripsi_tindakan = DeskripsiTindakan::where('deskripsi_tindakan', '=', $deskripsi_tindakan)->first();
+					if($data_deskripsi_tindakan == null) {
+						$data_deskripsi_tindakan = new DeskripsiTindakan();
+						$data_deskripsi_tindakan->deskripsi_tindakan = $deskripsi_tindakan;
+						$data_deskripsi_tindakan->save();
+					} 
+	
+					$data_tindakan_pasien = new DataTindakanPasien();
+					$data_tindakan_pasien->jp = $jp;
+					$data_tindakan_pasien->nama_dokter_perawat = $nama_dokter_perawat;
+					$data_tindakan_pasien->id_transaksi = $id_transaksi;
+					$data_tindakan_pasien->id_deskripsi_tindakan = $data_deskripsi_tindakan->id_deskripsi_tindakan;
+					$data_tindakan_pasien->save();
+				} else {
+					if($jp !== 0) {
+						$data_deskripsi_tindakan = DeskripsiTindakan::where('deskripsi_tindakan', '=', $deskripsi_tindakan)->first();
+						if($data_deskripsi_tindakan == null) {
+							$data_deskripsi_tindakan = new DeskripsiTindakan();
+							$data_deskripsi_tindakan->deskripsi_tindakan = $deskripsi_tindakan;
+							$data_deskripsi_tindakan->save();
+						} 
+	
+						$data_tindakan_pasien = new DataTindakanPasien();
+						$data_tindakan_pasien->jp = $jp;
+						$data_tindakan_pasien->nama_dokter_perawat = $nama_dokter_perawat;
+						$data_tindakan_pasien->id_transaksi = $id_transaksi;
+						$data_tindakan_pasien->id_deskripsi_tindakan = $data_deskripsi_tindakan->id_deskripsi_tindakan;
+						$data_tindakan_pasien->save();
 					}
 				}
 			}
-
-        	if($row[0] != "") {
-				$transaksi = new Transaksi();
-				$transaksi->id_data_pasien = $id_data_pasien;
-				$transaksi->no_sep = $no_sep;
-				$transaksi->reg_type = $reg_type;
-	        	$transaksi->id_users = Auth::user()->id_users;
-	        	$transaksi->id_periode = session('id_periode');
-	        	$transaksi->id_ruangan = session('id_ruangan');
-				$transaksi->save();
-
-				$id_transaksi = $transaksi->id_transaksi;
-
-    			$data_deskripsi_tindakan = DeskripsiTindakan::where('deskripsi_tindakan', '=', $deskripsi_tindakan)->first();
-    			if($data_deskripsi_tindakan == null) {
-    				$data_deskripsi_tindakan = new DeskripsiTindakan();
-    				$data_deskripsi_tindakan->deskripsi_tindakan = $deskripsi_tindakan;
-    				$data_deskripsi_tindakan->save();
-    			} 
-
-    			$data_tindakan_pasien = new DataTindakanPasien();
-    			$data_tindakan_pasien->jp = $jp;
-        		$data_tindakan_pasien->nama_dokter_perawat = $nama_dokter_perawat;
-        		$data_tindakan_pasien->id_transaksi = $id_transaksi;
-        		$data_tindakan_pasien->id_deskripsi_tindakan = $data_deskripsi_tindakan->id_deskripsi_tindakan;
-        		$data_tindakan_pasien->save();
-        	} else {
-        		if($jp !== 0) {
-        			$data_deskripsi_tindakan = DeskripsiTindakan::where('deskripsi_tindakan', '=', $deskripsi_tindakan)->first();
-        			if($data_deskripsi_tindakan == null) {
-        				$data_deskripsi_tindakan = new DeskripsiTindakan();
-        				$data_deskripsi_tindakan->deskripsi_tindakan = $deskripsi_tindakan;
-        				$data_deskripsi_tindakan->save();
-        			} 
-
-        			$data_tindakan_pasien = new DataTindakanPasien();
-        			$data_tindakan_pasien->jp = $jp;
-	        		$data_tindakan_pasien->nama_dokter_perawat = $nama_dokter_perawat;
-	        		$data_tindakan_pasien->id_transaksi = $id_transaksi;
-	        		$data_tindakan_pasien->id_deskripsi_tindakan = $data_deskripsi_tindakan->id_deskripsi_tindakan;
-	        		$data_tindakan_pasien->save();
-        		}
-        	}
         }
     }
 }
--- app/RekapData.php
+++ app/RekapData.php
@@ -22,21 +22,37 @@ class RekapData extends Model
 
         $hasil = [];
         $i = 0;
+        $tmp_upah_jasa = 0;
         foreach($data_dokter as $row){
-            $data_proses_perhitungan = DB::table('proses_perhitungan')
+            $hasil[$i]['id_dokter'] = $row->id_dokter;
+            $hasil[$i]['nama_dokter'] = $row->nama_dokter;
+            $data_proses_perhitungan_id_dokter = DB::table('proses_perhitungan')
             ->join('data_pasien', 'data_pasien.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
             ->join('transaksi', 'transaksi.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
             ->where('transaksi.id_periode', '=', $id)
             ->where('proses_perhitungan.id_dokter', '=', $row->id_dokter)
             ->where('proses_perhitungan.proses', '=', 'Ke 4')
             ->get();
-            $hasil[$i]['id_dokter'] = $row->id_dokter;
-            $hasil[$i]['nama_dokter'] = $row->nama_dokter;
-            $tmp_upah_jasa = 0;
-            foreach($data_proses_perhitungan as $row_perhitungan){
+            
+            
+            foreach($data_proses_perhitungan_id_dokter as $row_perhitungan){
                 $tmp_upah_jasa += $row_perhitungan->jumlah_jp;
             }
+
+            $data_proses_perhitungan_id_kat_dokter = DB::table('proses_perhitungan')
+            ->join('data_pasien', 'data_pasien.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
+            ->join('transaksi', 'transaksi.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
+            ->where('transaksi.id_periode', '=', $id)
+            ->where('proses_perhitungan.id_kategori_tindakan', '=', $row->id_kategori_tindakan)
+            ->where('proses_perhitungan.proses', '=', 'Ke 4')
+            ->get();
+            
+            foreach($data_proses_perhitungan_id_kat_dokter as $row_perhitungan_kat_dokter){
+                $tmp_upah_jasa += $row_perhitungan_kat_dokter->jumlah_jp;
+            }
+            
             $hasil[$i]['upah_jasa'] = $tmp_upah_jasa;
+            $tmp_upah_jasa = 0;
             $i++;
         }
         // dd($hasil);
@@ -45,16 +61,18 @@ class RekapData extends Model
 
     public function SelectRekapDataKategoriTindakanPerPeriode($id){
         $data_kategori_tindakan = DB::table('kategori_tindakan')
+        ->where('kategori_tindakan.kategori_data', '=', 'Penunjang')
         ->orderby('kategori_tindakan.id_kategori_tindakan', 'ASC')
         ->get();
 
         $hasil = [];
         $i = 0;
+        $tmp_upah_jasa = 0;
         foreach($data_kategori_tindakan as $row){
             $hasil[$i]['id_kategori_tindakan'] = $row->id_kategori_tindakan;
             $hasil[$i]['nama_kategori_tindakan'] = $row->nama;
             $hasil[$i]['bagian_kategori_tindakan'] = $row->kategori_data;
-            $tmp_upah_jasa = 0;
+            
             if($row->nama == 'GIZI')
             {
                 $data_proses_perhitungan = DB::table('proses_perhitungan')
@@ -85,6 +103,7 @@ class RekapData extends Model
             }
             
             $hasil[$i]['upah_jasa'] = $tmp_upah_jasa;
+            $tmp_upah_jasa = 0;
             $i++;
         }
         // dd($hasil);
@@ -98,52 +117,29 @@ class RekapData extends Model
 
         $hasil = [];
         $i = 0;
+        $tmp_upah_jasa = 0;
         foreach($data_ruangan as $row){
             $hasil[$i]['id_ruangan'] = $row->id_ruangan;
             $hasil[$i]['nama_ruangan'] = $row->nama_ruangan;
             $hasil[$i]['bagian'] = 'Perawat';
-            $tmp_upah_jasa = 0;
+            
             $data_proses_perhitungan_rpp = DB::table('proses_perhitungan')
             ->join('data_pasien', 'data_pasien.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
             ->join('transaksi', 'transaksi.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
             ->where('transaksi.id_periode', '=', $id)
-            ->where('proses_perhitungan.id_ruangan', '=', $row->id_ruangan)
+            // ->where('proses_perhitungan.id_ruangan', '=', $row->id_ruangan)
             ->where('proses_perhitungan.ket_kategori', '=', 'PERAWAT '.$row->nama_ruangan)
             ->where('proses_perhitungan.proses', '=', 'Ke 4')
             ->get();
             
             foreach($data_proses_perhitungan_rpp as $row_perhitungan){
                 $tmp_upah_jasa += $row_perhitungan->jumlah_jp;
-            }
-            // $data_proses_perhitungan_igd = DB::table('proses_perhitungan')
-            // ->join('data_pasien', 'data_pasien.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
-            // ->join('transaksi', 'transaksi.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
-            // ->where('transaksi.id_periode', '=', $id)
-            // ->where('proses_perhitungan.id_ruangan', '=', $row->id_ruangan)
-            // ->where('proses_perhitungan.ket_kategori', '=', 'PERAWAT IGD')
-            // ->where('proses_perhitungan.proses', '=', 'Ke 4')
-            // ->get();
-            
-            // foreach($data_proses_perhitungan_igd as $row_perhitungan){
-            //     $tmp_upah_jasa += $row_perhitungan->jumlah_jp;
-            // }
-            // $data_proses_perhitungan_iccu = DB::table('proses_perhitungan')
-            // ->join('data_pasien', 'data_pasien.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
-            // ->join('transaksi', 'transaksi.id_data_pasien', '=', 'proses_perhitungan.id_data_pasien')
-            // ->where('transaksi.id_periode', '=', $id)
-            // ->where('proses_perhitungan.id_ruangan', '=', $row->id_ruangan)
-            // ->where('proses_perhitungan.ket_kategori', '=', 'PERAWAT ICCU')
-            // ->where('proses_perhitungan.proses', '=', 'Ke 4')
-            // ->get();
-            
-            // foreach($data_proses_perhitungan_iccu as $row_perhitungan){
-            //     $tmp_upah_jasa += $row_perhitungan->jumlah_jp;
-            // }
-            
+            }            
             $hasil[$i]['upah_jasa'] = $tmp_upah_jasa;
+            $tmp_upah_jasa = 0;
             $i++;
         }
-        // dd($data_ruangan);
+        // dd($hasil);
         return $hasil;
     }
 
